9b9de410d7453226da3330434a1eafb97aa747b7
Igor Babuschkin <igor@babuschk.in>
2016-02-19 22:14:59 +0000

    Make BaseDistribution inherit from Tensor

--- tensorprob/distributions/base_distribution.py
+++ tensorprob/distributions/base_distribution.py
@@ -1,6 +1,13 @@
 from .. import utilities
+from .. import config
+import tensorflow as tf
 
-
-class BaseDistribution(object):
+class BaseDistribution(tf.Tensor):
     def __init__(self, name=None):
-        self.name = name or utilities.generate_name()
+        # Get the op of a new placeholder and use it as our op
+        placeholder = tf.placeholder(dtype=config.dtype, name=name or utilities.generate_name())
+        my_op = placeholder.op
+        my_op.outputs.clear()
+        my_op.outputs.append(self)
+
+        super(BaseDistribution, self).__init__(my_op, 0, config.dtype)
