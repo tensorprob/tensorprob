ddae495d9f521ef14c5f85e5eb9b51eac3616a37
Igor Babuschkin <igor@babuschk.in>
2016-02-20 02:16:16 +0000

    Add setup.py to project

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='tensorprob',
+      version='0.0.0',
+      description='A probabilistic graphical modeling framework based on Tensorflow',
+      url='http://ibab.github.io/tensorprob',
+      author='Igor Babuschkin',
+      author_email='igor@babuschk.in',
+      license='MIT',
+      install_requires=[
+          'tensorflow',
+      ],
+      packages=[
+          'tensorprob',
+          'tensorprob.distributions',
+      ],
+      zip_safe=False)
