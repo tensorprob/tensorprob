4c45cb9ae0ba4e0e24474f0323cbe7a94991b813
Igor Babuschkin <igor@babuschk.in>
2016-02-28 21:45:04 +0000

    Implement migrad as an optimization backend

--- tensorprob/optimizers/base.py
+++ tensorprob/optimizers/base.py
@@ -1,11 +1,71 @@
+import numpy as np
+import tensorflow as tf
+from .. import config
 
 class BaseOptimizer(object):
 
-    def __init__(self):
+    def __init__(self, session=None):
+        self._session = session or tf.Session()
+
+    def minimize_impl(self, objective, gradient, inits, bounds):
         raise NotImplementedError
 
-    def minimize(self, cost, gradient=None, variables=None):
-        raise NotImplementedError
+    def minimize(self, variables, cost, gradient=None, bounds=None):
+        # Check if variables is iterable
+        try:
+            iter(variables)
+        except TypeError:
+            raise ValueError("Variables parameter is not iterable")
+
+        for v in variables:
+            if not isinstance(v, tf.Variable):
+                raise ValueError("Parameter {} is not a tensorflow variable".format(v))
+
+        inits = self._session.run(variables)
+
+        def objective(xs):
+            feed_dict = {k: v for k, v in zip(variables, xs)}
+            # Cast just in case the user-supplied function returns something else
+            val = np.float64(self._session.run(cost, feed_dict=feed_dict))
+            if config.debug:
+                print('objective', val, xs)
+            return val
+
+        if gradient is not None:
+            def gradient_(xs):
+                feed_dict = {k: v for k, v in zip(variables, xs)}
+                # Cast just in case the user-supplied function returns something else
+                val = np.array(self._session.run(gradient, feed_dict=feed_dict))
+                if config.debug:
+                    print('gradient', val, xs)
+                return val
+            approx_grad = False
+        else:
+            gradient_ = None
+
+        if bounds:
+            min_bounds = []
+            for current in bounds:
+                lower, upper = current
+
+                # Translate inf to None, which is what this minimizer understands
+                if lower is not None and np.isinf(lower):
+                    lower = None
+                if upper is not None and np.isinf(upper):
+                    upper = None
+                # This optimizer likes to evaluate the function at the boundaries.
+                # Slightly move the bounds so that the edges are not included.
+                if lower is not None:
+                    lower = lower + 1e-10
+                if upper is not None:
+                    upper = upper - 1e-10
+
+                min_bounds.append((lower, upper))
+        else:
+            min_bounds = None
+
+        return self.minimize_impl(objective, gradient_, inits, min_bounds)
+
 
     @property
     def session(self):
--- tests/test_optimizer.py
+++ tests/test_optimizer.py
@@ -1,5 +1,5 @@
 
-from tensorprob import ScipyLBFGSBOptimizer
+from tensorprob import ScipyLBFGSBOptimizer, MigradOptimizer
 import numpy as np
 import tensorflow as tf
 from nose.tools import raises
@@ -10,16 +10,36 @@ def test_scipy_lbfgsb():
     sess.run(tf.initialize_variables([x]))
     optimizer = ScipyLBFGSBOptimizer(verbose=True, session=sess)
     # With gradient
-    results = optimizer.minimize([x], x**2, 2 * x)
+    results = optimizer.minimize([x], x**2, [2 * x])
     assert results.success
     # Without gradient
-    results = optimizer.minimize([x], 2 * x)
+    results = optimizer.minimize([x], x**2)
     assert results.success
     # Test callback
     def callback(xs):
         pass
     optimizer = ScipyLBFGSBOptimizer(verbose=True, session=sess, callback=callback)
     assert optimizer.minimize([x], x**2).success
+    @raises(ValueError)
+    def test_illegal_parameter_as_variable1():
+        optimizer.minimize([42], x**2)
+    test_illegal_parameter_as_variable1()
+    @raises(ValueError)
+    def test_illegal_parameter_as_variable2():
+        optimizer.minimize(42, x**2)
+    test_illegal_parameter_as_variable2()
+
+def test_migrad():
+    sess = tf.Session()
+    x = tf.Variable(np.float64(2), name='x')
+    sess.run(tf.initialize_variables([x]))
+    optimizer = MigradOptimizer(session=sess)
+    # With gradient
+    results = optimizer.minimize([x], x**2, [2 * x])
+    assert results.success
+    # Without gradient
+    results = optimizer.minimize([x], x**2)
+    assert results.success
     @raises(ValueError)
     def test_illegal_parameter_as_variable1():
         optimizer.minimize([42], x**2)
